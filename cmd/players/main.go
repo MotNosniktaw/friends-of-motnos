9d041a85a314759334cdd23ab903c0b1a68458d8
MotNosniktaw <tomwatkinsondeveloper@gmail.com>
2023-11-16 22:36:22 +0000

    add counter

--- cmd/players/main.go
+++ cmd/players/main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
+
+var counter = 0
 
 func main() {
 	r := http.NewServeMux()
 
 	r.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("pong"))
+	})
+
+	r.HandleFunc("/counter", func(w http.ResponseWriter, r *http.Request) {
+		counter++
+		w.Write([]byte(strconv.Itoa(counter)))
 	})
 
 	fmt.Println("running players service")
