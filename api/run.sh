cf01266871abae41d8684fa201eb04e91ca16392
MotNosniktaw <tomwatkinsondeveloper@gmail.com>
2023-11-25 11:49:43 +0000

    rm db dovker on local

--- api/run.sh
+++ api/run.sh
@@ -1,1 +1,1 @@
-docker run --name fom --mount source=fom,target=/app -e POSTGRES_DB=fom -e POSTGRES_PASSWORD=password -p 5432:5432 postgis/postgis:latest
+docker run --rm --name fom --mount source=fom,target=/app -e POSTGRES_DB=fom -e POSTGRES_PASSWORD=password -p 5432:5432 postgis/postgis:latest
